8b226187052700f17128cdb71f1023d9fd1cdeef
4vm <amadeumrzr@gmail.com>
2026-03-21 12:54:32 -0300

    correcao PostRepository

--- src/repositories/PostRepository.ts
+++ src/repositories/PostRepository.ts
@@ -1,46 +1,71 @@
 import { prisma } from "../client";
 
 export class PostRepository {
-
-    async findAll() { return prisma.post.findMany(); }
-
-    async findPublished() {
-        return prisma.post.findMany({
-            where: { published: true }
-        });
-    }
-
-    async findById(id: string) { return prisma.post.findUnique({ where: { id } }); }
-
-    async create(data: { title: string; content: string; authorId: string; published: boolean }) {
-        return prisma.post.create({ data });
-    }
-
-    async update(id: string, data: { title: string; content: string, published: boolean }) {
-        return prisma.post.update({ where: { id }, data });
-    }
-
-async delete(id: string) {
-  const result = await prisma.post.deleteMany({
-    where: { id }
-  });
-
-  if (result.count === 0) {
-    throw new Error("Post não encontrado");
+  async findAll() {
+    return prisma.post.findMany({
+      include: {
+        author: {
+          select: { name: true, id: true }
+        }
+      }
+    });
   }
 
-  return result;
-}
+  async findPublished() {
+    return prisma.post.findMany({
+      where: { published: true },
+      include: {
+        author: {
+          select: { name: true }
+        }
+      }
+    });
+  }
 
+  async findById(id: string) {
+    return prisma.post.findUnique({
+      where: { id },
+      include: {
+        author: {
+          select: { name: true }
+        }
+      }
+    });
+  }
 
-    async searchByKeyword(keyword: string) {
-        return prisma.post.findMany({
-            where: {
-                OR: [
-                    { title: { contains: keyword, mode: "insensitive" } },
-                    { content: { contains: keyword, mode: "insensitive" } }
-                ]
-            }
-        });
+  async create(data: { title: string; content: string; authorId: string; published: boolean }) {
+    return prisma.post.create({ data });
+  }
+
+  async update(id: string, data: { title: string; content: string, published: boolean }) {
+    return prisma.post.update({ where: { id }, data });
+  }
+
+  async delete(id: string) {
+    const result = await prisma.post.deleteMany({
+      where: { id }
+    });
+
+    if (result.count === 0) {
+      throw new Error("Post não encontrado");
     }
+
+    return result;
+  }
+
+  async searchByKeyword(keyword: string) {
+    return prisma.post.findMany({
+      where: {
+        OR: [
+          { title: { contains: keyword, mode: "insensitive" } },
+          { content: { contains: keyword, mode: "insensitive" } }
+        ]
+      },
+      include: {
+        author: {
+          select: { name: true }
+        }
+      }
+    });
+  }
 }
